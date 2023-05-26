68f6d4b8d625a2919f7e80bab0822e0c809d1e5b
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-05-26 12:47:18 +0530

    INitial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//JAVASCRIPT
+
+// const heading = document.createElement("h2");
+// heading.textContent = "Hello World";
+// heading.className = "header";
+// document.getElementById("root").append(heading);
+
+// REACT with JS
+// const reactHeading = React.createElement("h1",{className : "head"},"Hellow Reactṣ!!!")
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
+// React with JSX
+// const jsxHeading =<React.Fragment>
+//                 <h1 className = "head">Hellow World!</h1>
+//                 <p>This is created using JSX</p>
+//                 </React.Fragment> 
+// ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+
+
+//creating the list with JSX
+// const jsxHeading = <div>
+//                     <h1>About React</h1>
+//                     <ul>
+//                         <li>eer</li>
+//                         <li>dknd</li>
+//                         <li>dfkdfk</li>
+//                     </ul>
+//                     </div>
+// ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+
+// Creating the component in React
+// function App(){
+//     return(
+//         <>
+//         <h1>Hello</h1>
+//         <p>World</p>
+//         </>
+//     )
+// }
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<h1>This a test</h1>);
